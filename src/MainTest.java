877641d4a337cbf1145b938fd3afbb7609fe5699
E. Amblard <amblard@3il.fr>
2019-05-16 11:08:51 +0200

    Fichiers initiaux

---
+++ src/MainTest.java
@@ -0,0 +1,18 @@
+
+public class MainTest {
+
+	public static void main(String[] args) {
+
+		String[] villes = {
+				"Limoges",
+				"Paris",
+				"Vic",
+		};
+		
+		for ( String ville : villes ) {
+			System.out.println( ville );
+		}
+
+	}
+
+}
