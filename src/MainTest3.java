24827fee70220a2df25671f0a500f3643ddfa1df
E. Amblard <amblard@3il.fr>
2019-05-16 15:13:48 +0200

    ManiTest3

---
+++ src/MainTest3.java
@@ -0,0 +1,20 @@
+
+public class MainTest3 {
+
+	public static void main(String[] args) {
+
+		String[] villes = {
+				"Limoges 3",
+				"Paris 1+2+3",
+				"Victoire 1+2+3",
+				"Vic-le-Comte",
+				"Saint-Vic",
+		};
+		
+		for ( String ville : villes ) {
+			System.out.println( ville );
+		}
+
+	}
+
+}
